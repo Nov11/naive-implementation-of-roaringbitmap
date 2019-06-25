bf362a428eab9b7007a84623331163048b801186
Nov11 <529079634@qq.com>
2019-06-25 23:15:28 +0800

    + coverall

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,5 @@ go get github.com/mattn/goveralls
 
 
 go build -v ./...
-go test -v -covermode=count -coverprofile=coverage.out
-$HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
+go test -v -covermode=count -coverprofile=coverage.out ./...
+$HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $repotoken
