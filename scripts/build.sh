4f4bb5ae76942a88b28c9592f9a40292d87743ae
Nov11 <529079634@qq.com>
2019-06-25 23:13:22 +0800

    + coverall

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export GO111MODULE=on
+
 go get golang.org/x/tools/cmd/cover
 go get github.com/mattn/goveralls
 
-export GO111MODULE=on
+
 go build -v ./...
 go test -v -covermode=count -coverprofile=coverage.out
 $HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
